9d6fc62fb967f71d081379c477de2c9c4f5f82da
Colin Rubow <colinwrubow@gmail.com>
2026-01-01 23:08:34 -0700

    box permutation

--- permutation_cipher.py
+++ permutation_cipher.py
@@ -108,3 +108,76 @@ def permute_mod_class_decrypt_exhaustive(ciphertext: list[str]) -> tuple[list[st
         if is_valid(plaintext, dictionary, max_word_len, 20):
             return list(plaintext), key
     return [""], 0
+
+def permute_box_encryption(plaintext: list[str], m: int, n: int) -> list[str]:
+    """
+    encrypts by making the message into blocks of m rows and n columns
+
+    Arguments
+    ---------
+    plaintext: the message
+    m: the number of rows
+    n: the number of columns
+
+    Returns
+    -------
+    the ciphertext
+    """
+    ciphertext = []
+    for i in range(0, len(plaintext), m*n):
+        ciphertext.extend(permute_mod_class_encrypt(plaintext[i:i+m*n], n))
+    return ciphertext
+
+def permute_box_decryption(ciphertext: list[str], m: int, n: int) -> list[str]:
+    """
+    decrypts by making the message into blocks of m rows and n columns
+
+    Arguments
+    ---------
+    ciphertext: the message
+    m: the number of rows
+    n: the number of columns
+
+    Returns
+    -------
+    the plaintext
+    """
+    plaintext = []
+    for i in range(0, len(ciphertext), m*n):
+        plaintext.extend(permute_mod_class_decrypt(ciphertext[i:i+m*n], n))
+    return plaintext
+
+def permute_box_decryption_exhaustive(ciphertext: list[str]) -> tuple[list[str], int, int]:
+    """
+    decrypts a box permutation cipher by exhaustion
+
+    Arguments
+    ---------
+    ciphertext: the message
+
+    Returns
+    -------
+    the plaintext, m, and n (the key)
+    """
+    dictionary, max_word_len = get_dictionary('./dictionary.txt')
+    for i in range(2, 11):
+        for j in range(2, 11):
+            plaintext = permute_box_decryption(ciphertext, i, j)
+            if is_valid(''.join(plaintext), dictionary, max_word_len, 20):
+                return plaintext, i, j
+    return [''], 0, 0
+
+
+if __name__ == '__main__':
+    plaintext = list('cryptography')
+    ciphertext = permute_box_encryption(plaintext, 3, 4)
+    assert ''.join(ciphertext) == 'ctaropyghpry'
+
+    plaintext = permute_box_decryption(ciphertext, 3, 4)
+    assert ''.join(plaintext) == 'cryptography'
+
+    ciphertext = list('myamraruyiqtenctorahroywdsoyeouarrgdernogw')
+    plaintext, m, n = permute_box_decryption_exhaustive(ciphertext)
+    print(''.join(plaintext))
+    print(m)
+    print(n)
